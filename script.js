ebca9c201077d96c69353a4d62a49cd587f29129
Abhishek Haikerwal <abhishekhaikerwal24@gmail.com>
2024-06-14 17:32:37 +0530

    list traversing added in dropdown list

--- script.js
+++ script.js
@@ -6,14 +6,64 @@ const apiKey = "1fc0f2a755f22e3a3d85fe668df0b5aa";
 const searchBtn = document.getElementById("searchBtn");
 const search = document.getElementById("search");
 const list = document.getElementById("dropdown");
+let idx = -1;
 
 
-// const getData(url){
+const getWeatherData = (lat,lon) =>{
+    let days16DataUrl = `api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-// }
+    fetch(url)
+    .then(response =>{
+        if(!response.ok){
+            throw new Error("Network response was not ok");
+        }
+        return response.json();
+    })
+    .then(data =>{
+        console.log(data);
+    })
+    .catch(error =>{
+        alert("There was a problem check console!");
+        console.log(error);
+    });
+};
+
+const getData = (url)=>{
+    fetch(url)
+    .then(response =>{
+        if(!response.ok){
+            throw new Error("Network response was not ok");
+            alert("Network Error")
+        }
+        return response.json();
+    })
+    .then(data =>{
+        if(data.length===0){
+            alert("city not found!");
+        }
+        else if(!data[0].name){
+            alert("city not found!");
+        }else{
+            const {name,lat,lon} = data[0];
+            getWeatherData(name,lat,lon);
+        }
+    })
+    .catch(error =>{
+        alert("There was a problem check console!");
+        console.log(error);
+        
+    });
+};
 
 const getCordinates = (value) =>{
-    const geoCodingUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=1&appid=${apiKey}`;
+    if(value){
+        let city = value[0].toLocaleUpperCase()+value.slice(1);
+        let geoCodingUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+        getData(geoCodingUrl);
+        // alert(city);
+    }else{
+        alert("enter a city");
+    }    // alert(value);
     
 };
 
@@ -83,27 +133,54 @@ const display = () =>{
 
     listItem.forEach(element =>{
         element.addEventListener("click",(event) =>{
+            list.classList.add("hidden");
+            // console.log(list.children.length);
             search.value = event.target.classList[1];
-        })
+        });
+
     });
     
 };
 
-searchBtn.addEventListener("click",()=> {
+
+const updateSelection = (Nidx) => {
+    const listItems = document.querySelectorAll(".listItem");
+    if (idx !== -1) {
+        // console.log(idx);
+        listItems[idx].classList.remove("selected");
+    }
+    idx = Nidx;
+    if (idx !== -1) {
+        // console.log(Nidx);
+        listItems[idx].classList.add("selected");
+        listItems[idx].focus();
+    }
+};
+
+searchBtn.addEventListener("click",(event)=> {
     const city = document.getElementById("search").value;
     const data = localStorage.getItem("places");
     store(city.toLocaleLowerCase().trim());
     getCordinates(search.value);
     search.value="";
+
+    
 });
 
 search.addEventListener("keydown",(event)=>{
+    const listItems = document.querySelectorAll(".listItem");
     if (event.key === "Enter"){
-        const city = document.getElementById("search").value;
-        const data = localStorage.getItem("places");
-        store(city.toLocaleLowerCase().trim());
-        getCordinates(search.value);
-        search.value="";
+        if (event.key === "Enter" && idx !== -1) {
+            search.value = listItems[idx].classList[1];
+            idx=-1;
+        }else{
+            const city = document.getElementById("search").value;
+            const data = localStorage.getItem("places");
+            store(city.toLocaleLowerCase().trim());
+            getCordinates(search.value);
+            search.value="";
+        }
+        
     }
 
     let data = localStorage.getItem("places");
@@ -111,6 +188,23 @@ search.addEventListener("keydown",(event)=>{
     if(data.length !== 0 ){
         list.innerHTML="";
         display();
+    }
+
+     // Handle arrow key navigation
+     if (event.key === "ArrowDown") {
+        event.preventDefault();
+        if (idx < listItems.length-1) {
+            updateSelection(idx + 1);
+        }else{
+            updateSelection(0)
+        }
+    } else if (event.key === "ArrowUp") {
+        event.preventDefault();
+        if (idx >0) {
+            updateSelection(idx - 1);
+        }else{
+            updateSelection(listItems.length-1)
+        }
     }
 });
 
@@ -120,13 +214,14 @@ search.addEventListener("mouseenter",()=>{
     if(data.length !== 0 ){
         list.innerHTML="";
         display();
-    }
+    };
 });
 
 document.addEventListener("click",(event)=>{
     if (!search.contains(event.target) && !list.contains(event.target)) { // Check if click is outside the list and search button
         if (!list.classList.contains("hidden")) {
             list.classList.add("hidden");
+            idx=-1;
         }
     }
 });
